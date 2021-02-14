111c6c3747726f97ccc431c142816fed168b2eb5
ryowright <ryow.college@gmail.com>
2021-02-13 19:25:27 -0800

    added react build to index.js in api

--- api/src/index.js
+++ api/src/index.js
@@ -1,5 +1,6 @@
 require('./db/mongoose');
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 const userRouter = require('./routes/user');
 const goalRouter = require('./routes/goal');
@@ -8,6 +9,8 @@ require('dotenv').config();
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+app.use(express.static(path.join(__dirname, '../../dist')))
+
 app.use(express.json());
 app.use(cors());
 app.use('/api/user', userRouter);
@@ -15,5 +18,8 @@ app.use('/api/goal', goalRouter);
 // app.route('/*').get(function(req, res) {
 //     return res.sendFile(path.join(__dirname, '../../frontend/src/index.html'))
 // })
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../dist/index.html'));
+});
 
 app.listen(PORT);
