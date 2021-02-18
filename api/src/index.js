60d59e5d3cbdde2f8bb3e7935f739b818ac19206
ryowright <ryow.college@gmail.com>
2021-02-17 23:36:52 -0800

    Fixed settings route issue

--- api/src/index.js
+++ api/src/index.js
@@ -18,7 +18,7 @@ app.use('/api/goal', goalRouter);
 // app.route('/*').get(function(req, res) {
 //     return res.sendFile(path.join(__dirname, '../../frontend/src/index.html'))
 // })
-app.get('/', (req, res) => {
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '../../dist/index.html'));
 });
 
